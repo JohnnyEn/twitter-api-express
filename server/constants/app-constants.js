83803d57ee670292646b03f4c10c2fed5896ae44
johnny <johnny@psytrance-radio.cz>
2020-12-17 13:50:27 +0100

    master - initial commit

---
+++ server/constants/app-constants.js
@@ -0,0 +1,8 @@
+// TWITTER V1 Endpoints
+const TWITTER_V1_REQUEST_OAUTH_TOKEN = 'https://twitter.com/oauth/request_token';
+const TWITTER_V1_ACCESS_OAUTH_TOKEN = 'https://twitter.com/oauth/access_token';
+
+// TWITTER V2 Endpoints
+const TWITTER_V2_OAUTH_URL = 'https://api.twitter.com/oauth2/token';
+const TWITTER_V2_RULES_URL = 'https://api.twitter.com/2/tweets/search/stream/rules';
+const TWITTER_V2_STREAM_URL = 'https://api.twitter.com/2/tweets/search/stream';
